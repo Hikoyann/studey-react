4944aeb7cd647c5f21e5f0529c5c49e4fb7292e7
Hikoyann <i21i022dl@ous.jp>
2023-08-30 23:24:28 +0900

    ♻️ Main部分のコンポーネント化

--- components/Headline.js
+++ components/Headline.js
@@ -6,14 +6,10 @@ import styles from "@/styles/Home.module.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export function Headline(props) {
-  console.log(props.title);
   return (
     <>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>pages/{props.page}.js</code>
-        </p>
+        <p>Get started by editing&nbsp;{props.children}</p>
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
