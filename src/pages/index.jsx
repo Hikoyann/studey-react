7a1e672b02d3e3c307b6a43d78c571d7ffb9dd78
Hikoyann <i21i022dl@ous.jp>
2023-09-01 22:56:41 +0900

    ⚗️ 不要な記述、いつか消す

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,16 +2,32 @@ import Head from "next/head";
 import { Inter } from "next/font/google";
 import { Main } from "@/src/components/Main";
 import { Header } from "@/src/components/Header";
+import { useCallback } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
+// const handleClick = (e) => {
+//   console.log(e.target.href);
+//   e.preventDefault();
+// };
+
 export default function Home() {
+  const foo = 1;
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo);
+  }, []);
+
   return (
     <>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
+      <a href="/about" onClick={handleClick}>
+        ボタン
+      </a>
       <Main page="index" />
     </>
   );
