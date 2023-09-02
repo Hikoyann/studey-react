e8a4889d9a804e40300888a90bd60d1b123f3e5f
Hikoyann <i21i022dl@ous.jp>
2023-09-02 13:44:18 +0900

    ✨ カウントを設置し、useStateでカウントアップの機能追加

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import { Inter } from "next/font/google";
 import { Main } from "@/src/components/Main";
 import { Header } from "@/src/components/Header";
-import { useCallback, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,19 +12,19 @@ const inter = Inter({ subsets: ["latin"] });
 // };
 
 export default function Home() {
-  const foo = 1;
-  const handleClick = useCallback((e) => {
-    console.log(e.target.href);
-    e.preventDefault();
-    alert(foo);
-  }, []);
+  const [count, setFoo] = useState(1);
+  // let count = 1;
+
+  const handleClick = (e) => {
+    setFoo((count) => count + 1);
+    setFoo((count) => count + 1);
+    // count = count + 1;
+  };
 
   useEffect(() => {
-    console.log("マウント時");
     document.body.style.backgroundColor = "lightblue";
 
     return () => {
-      console.log("アンマウント時");
       document.body.style.backgroundColor = "";
     };
   }, []);
@@ -35,9 +35,8 @@ export default function Home() {
         <title>Index Page</title>
       </Head>
       <Header />
-      <a href="/about" onClick={handleClick}>
-        ボタン
-      </a>
+      <h1>{count}</h1>
+      <button onClick={handleClick}>ボタン</button>
       <Main page="index" />
     </>
   );
