659bda970e424e2e90ac40e22b7d59e779ca81fc
Hikoyann <i21i022dl@ous.jp>
2023-09-13 22:49:24 +0900

    ✨ useMemoについて学習

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function About(props) {
   const {
-    count,
+    doubleCount,
     isShow,
     handleClick,
     handleDisplay,
@@ -24,7 +24,7 @@ export default function About(props) {
       </Head>
       <Header />
 
-      {isShow ? <h1>{count}</h1> : null}
+      {isShow ? <h1>{doubleCount}</h1> : null}
       <button onClick={handleClick}>ボタン</button>
       <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
 
