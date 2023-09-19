e1a1f0b07f5c3e9ae97b06ad458266bd5ffc9108
Hikoyann <i21i022dl@ous.jp>
2023-09-19 12:31:05 +0900

    🚸 APIのローディング状態とエラーの追加

---
+++ src/components/Posts/index.jsx
@@ -0,0 +1,52 @@
+import { Inter } from "next/font/google";
+import { useCallback, useEffect, useState } from "react";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const Posts = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const getPosts = useCallback(async () => {
+    try {
+      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+      if (!res.ok) {
+        throw new Error("エラーが発生したため、データの取得に失敗");
+      }
+      const json = await res.json();
+      setPosts(json);
+    } catch (error) {
+      setError(error);
+    }
+    setLoading(false);
+  }, []);
+
+  useEffect(() => {
+    getPosts();
+  }, [getPosts]);
+
+  if (loading) {
+    return <div>ローディング中</div>;
+  }
+
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+
+  if (posts.length === 0) {
+    return <div>データは空です</div>;
+  }
+
+  return (
+    <>
+      <ol>
+        {posts.map((post) => {
+          return <li key={post.id}>{post.title}</li>;
+        })}
+      </ol>
+    </>
+  );
+};
+
+export default Posts;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,42 +1,18 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
-import { Main } from "@/src/components/Main";
 import { Header } from "@/src/components/Header";
-import { useCounter } from "../hooks/useCounter";
-import { useInputArray } from "../hooks/useInputArray";
-import { useBgLightBlue } from "../hooks/useBgColor";
-import { useCallback, useEffect, useState } from "react";
+import Posts from "../components/Posts";
 
 const inter = Inter({ subsets: ["latin"] });
 
-const Home = (props) => {
-  const [posts, setPosts] = useState([]);
-
-  const getPosts = useCallback(async () => {
-    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-    const json = await res.json();
-    setPosts(json);
-  }, []);
-
-  useEffect(() => {
-    getPosts();
-  }, [getPosts]);
-  console.log(posts);
-
+const Home = () => {
   return (
     <>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
-
-      {posts.length > 0 ? (
-        <ol>
-          {posts.map((post) => {
-            return <li key={post.id}>{post.title}</li>;
-          })}
-        </ol>
-      ) : null}
+      <Posts />
     </>
   );
 };
