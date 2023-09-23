df34385c0ca9021ddcd6823f0c539148ef759112
Hikoyann <i21i022dl@ous.jp>
2023-09-23 11:56:17 +0900

    ⚡️ useReducerを使いわかりやすいコードに変更

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,41 +1,51 @@
 import { Inter } from "next/font/google";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useState, useReducer } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const initialState = {
+  data: [],
+  loading: true,
+  error: null,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "end": {
+      return {
+        ...state,
+        data: action.data,
+        loading: false,
+      };
+    }
+
+    case "error": {
+      return {
+        ...state,
+        loading: false,
+        error: action.error,
+      };
+    }
+
+    default: {
+      throw new Error("no such action type!");
+    }
+  }
+};
+
 export const Posts = () => {
-  // const [posts, setPosts] = useState([]);
-  // const [loading, setLoading] = useState(true);
-  // const [error, setError] = useState(null);
-  const [state, setState] = useState({
-    data: [],
-    loading: true,
-    error: null,
-  });
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const getPosts = useCallback(async () => {
     try {
-      const res = await fetch("https://jsonplaceholder.typicode.com/postsaa");
+      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
       if (!res.ok) {
         throw new Error("エラーが発生したため、データの取得に失敗");
       }
       const json = await res.json();
-      // setPosts(json);
-      setState((prevState) => {
-        return {
-          ...prevState,
-          data: json,
-          loading: false,
-        };
-      });
+      dispatch({ type: "end", data: json });
     } catch (error) {
-      setState((prevState) => {
-        return {
-          ...prevState,
-          loading: false,
-          error,
-        };
-      });
+      dispatch({ type: "error", error });
     }
   }, []);
 
