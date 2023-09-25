4b77f643dd4a1ac475ace0e4df8b7b80b7e32e5f
Hikoyann <i21i022dl@ous.jp>
2023-09-25 23:46:57 +0900

    ⚡️ useSWRにfetcherを追加、たまにwebAPIにエラーが出る

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,63 +1,26 @@
 import { Inter } from "next/font/google";
-import { useCallback, useEffect, useState, useReducer } from "react";
+// import { useCallback, useEffect, useState, useReducer } from "react";
 import useSWR from "swr";
 
 const inter = Inter({ subsets: ["latin"] });
 
-const initialState = {
-  data: [],
-  loading: true,
-  error: null,
-};
+const fetcher = async (url) => {
+  const response = await fetch(url);
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "end": {
-      return {
-        ...state,
-        data: action.data,
-        loading: false,
-      };
-    }
-
-    case "error": {
-      return {
-        ...state,
-        loading: false,
-        error: action.error,
-      };
-    }
-
-    default: {
-      throw new Error("no such action type!");
-    }
+  if (!response.ok) {
+    throw new Error("エラーが発生したため、データの取得に失敗しました。");
   }
+
+  const json = await response.json();
+  return json;
 };
 
 export const Posts = () => {
-  const { data, error } = useSWR("https://jsonplaceholder.typicode.com/posts");
+  const { data, error } = useSWR(
+    "https://jsonplaceholder.typicode.com/posts",
+    fetcher
+  );
   console.log({ data, error });
-
-  // const [state, dispatch] = useReducer(reducer, initialState);
-
-  // const getPosts = useCallback(async () => {
-  //   try {
-  //     const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-  //     if (!res.ok) {
-  //       throw new Error("エラーが発生したため、データの取得に失敗");
-  //     }
-  //     const json = await res.json();
-  //     dispatch({ type: "end", data: json });
-  //   } catch (error) {
-  //     dispatch({ type: "error", error });
-  //   }
-  // }, []);
-
-  // useEffect(() => {
-  //   getPosts();
-  // }, [getPosts]);
-
-  console.log("foo");
 
   if (!error && !data) {
     return <div>ローディング中</div>;
