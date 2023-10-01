055d2657c93f17e224ddd08391ec10b05ce35a81
Hikoyann <i21i022dl@ous.jp>
2023-10-01 23:55:44 +0900

    🔥 Aboutファイルのコード削除

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,14 +1,7 @@
 import "src/styles/globals.css";
 import Head from "next/head";
-import { useCounter } from "../hooks/useCounter";
-import { useInputArray } from "../hooks/useInputArray";
-import { useBgColor } from "../hooks/useBgColor";
 
 const MyApp = ({ Component, pageProps }) => {
-  const counter = useCounter();
-  const inputArray = useInputArray();
-  useBgColor();
-
   return (
     <>
       <Head>
@@ -16,7 +9,7 @@ const MyApp = ({ Component, pageProps }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Component {...pageProps} {...counter} {...inputArray} />
+      <Component {...pageProps} />
     </>
   );
 };
