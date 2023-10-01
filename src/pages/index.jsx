97aec6271b0bfb06521ceeb6ca5ca63b2bf36179
Hikoyann <i21i022dl@ous.jp>
2023-10-01 23:52:39 +0900

    🔥 Aboutファイルのコード削除<Mian />を消した

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
-import { Main } from "@/src/components/Main";
 import { Header } from "@/src/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -9,25 +8,11 @@ const About = (props) => {
   return (
     <>
       <Head>
-        <title>About Page</title>
+        <title>Index Page</title>
       </Head>
       <Header />
-
-      {props.isShow ? <h1>{props.doubleCount}</h1> : null}
-      <button onClick={props.handleClick}>ボタン</button>
-      <button onClick={props.handleDisplay}>
-        {props.isShow ? "非表示" : "表示"}
-      </button>
-
-      <input type="text" value={props.text} onChange={props.handleChange} />
-      <button onClick={props.handleAdd}>追加</button>
-      <ul>
-        {props.array.map((item) => {
-          return <li key={item}>{item}</li>;
-        })}
-      </ul>
-
-      <Main page="about" />
+      <h1>Next.jsでReactの学習</h1>
+      <p>JsonPlaceholderのAPIを叩いて学習</p>
     </>
   );
 };
