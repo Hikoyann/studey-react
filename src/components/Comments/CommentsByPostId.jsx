00bcb5a4d58af7319ab268159d2e0da374cfda3f
Hikoyann <i21i022dl@ous.jp>
2023-10-06 12:54:15 +0900

    ✨ Postのコメントに紐付いたCommentsを表示する機能を追加

---
+++ src/components/Comments/CommentsByPostId.jsx
@@ -0,0 +1,33 @@
+import { useCommentsByPostsId } from "@/src/hooks/useFetchArray";
+import Link from "next/link";
+
+export const CommentsByPostId = (props) => {
+  const { data, error, isLoading, isEmpty } = useCommentsByPostsId(props.id);
+
+  if (isLoading) {
+    return <div>ローディング中</div>;
+  }
+
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+
+  if (isEmpty) {
+    return <p>No comments found.</p>;
+  }
+
+  return (
+    <ol>
+      {data.map((comment) => {
+        return (
+          <li key={comment.id}>
+            <Link href={`/comments/${comment.id}`}>
+              {comment.id + "："}
+              {`${comment.body}`}
+            </Link>
+          </li>
+        );
+      })}
+    </ol>
+  );
+};
--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,12 +1,12 @@
 import { usePost } from "@/src/hooks/usePost";
 import { Inter } from "next/font/google";
 import Head from "next/head";
+import { CommentsByPostId } from "../Comments/CommentsByPostId";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const Post = () => {
   const { post, user, error, isLoading } = usePost();
-  // console.log({ data, error });
 
   if (isLoading) {
     return <div>ローディング中</div>;
@@ -25,6 +25,7 @@ export const Post = () => {
         <h1>{post?.title}</h1>
         <p>{post?.body}</p>
         {user?.name ? <div>Created by{user?.name}</div> : null}
+        <CommentsByPostId id={post.id} />
       </div>
     </>
   );
