149059b68b1cf2be4fe8a659f721ecfab47bc194
Hikoyann <i21i022dl@ous.jp>
2023-10-07 21:10:34 +0900

    🎨 投稿・取得周りのアーキテクチャを変更

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -3,11 +3,13 @@ import { Inter } from "next/font/google";
 import Head from "next/head";
 import { CommentsByPostId } from "../Comments/CommentsByPostId";
 import { UserByUserId } from "../User/UserByPostId";
+import { useRouter } from "next/router";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const Post = () => {
-  const { data, error, isLoading } = usePost();
+  const router = useRouter();
+  const { data, error, isLoading } = usePost(router.query.id);
 
   if (isLoading) {
     return <div>ローディング中</div>;
