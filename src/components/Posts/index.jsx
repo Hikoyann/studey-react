244a08f897739852aa0a022f1e3b04218ab95937
Hikoyann <i21i022dl@ous.jp>
2023-10-08 20:38:55 +0900

    💄 Tailwind CSSでデザインの作成、まだ作成途中のため、次回続けて作成

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -20,19 +20,24 @@ export const Posts = () => {
   }
 
   return (
-    <>
-      <ol>
-        {data.map((post) => {
-          return (
-            <li key={post.id}>
-              <Link href={`/posts/${post.id}`}>
-                {post.id}：{post.title}
-              </Link>
-            </li>
-          );
-        })}
-      </ol>
-    </>
+    <ul className="space-y-4">
+      {data.map((post) => {
+        return (
+          <li key={post.id}>
+            <Link href={`/posts/${post.id}`}>
+              <div className="block group">
+                <h1 className="font-bold group-hover:text-blue-500">
+                  {post.title}
+                </h1>
+                <p className="text-sm text-gray-500 group-hover:text-blue-500">
+                  {post.body}
+                </p>
+              </div>
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
   );
 };
 
--- src/components/Users/index.jsx
+++ src/components/Users/index.jsx
@@ -15,16 +15,19 @@ export const UsersComponent = () => {
   }
 
   return (
-    <ol>
+    <ul className="grid grid-cols-3 gap-4">
       {data.map((user) => {
         return (
           <li key={user.id}>
             <Link href={`/users/${user.id}`}>
-              {`${user.name}` + "：" + `(${user.email})`}
+              <div className="block p-2 shadow rounded hover:bg-gray-100">
+                <h1 className="font-bold">{user.name}</h1>
+                <div className="text-sm">{user.email}</div>
+              </div>
             </Link>
           </li>
         );
       })}
-    </ol>
+    </ul>
   );
 };
