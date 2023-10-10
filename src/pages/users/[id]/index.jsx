b246a66e3e68a2ad4cd82065248d955698d3a957
Hikoyann <i21i022dl@ous.jp>
2023-10-10 21:05:07 +0900

    ✨ UserページにSSRを実践

--- src/pages/users/[id]/index.jsx
+++ src/pages/users/[id]/index.jsx
@@ -1,15 +1,32 @@
 import { Inter } from "next/font/google";
 import { Header } from "@/src/components/Header";
 import { UserComponent } from "@/src/components/User";
+import { SWRConfig } from "swr";
 
 const inter = Inter({ subsets: ["latin"] });
 
-const UsersId = () => {
+export const getServerSideProps = async (ctx) => {
+  const { id } = ctx.query;
+  const API_URL = `https://jsonplaceholder.typicode.com/users/${id}`;
+  const user = await fetch(API_URL);
+  const userData = await user.json();
+
+  return {
+    props: {
+      fallback: {
+        [API_URL]: userData,
+      },
+    },
+  };
+};
+
+const UsersId = (props) => {
+  const { fallback } = props;
   return (
-    <>
+    <SWRConfig value={{ fallback }}>
       <Header />
       <UserComponent />
-    </>
+    </SWRConfig>
   );
 };
 
