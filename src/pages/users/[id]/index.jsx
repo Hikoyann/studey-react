5b499709a09b971116ec57ee7ce30a5ebbf25490
Hikoyann <i21i022dl@ous.jp>
2023-10-10 21:41:34 +0900

    ⚡️ SSRの設定とSWRでイミュータブルに変更

--- src/pages/users/[id]/index.jsx
+++ src/pages/users/[id]/index.jsx
@@ -7,14 +7,20 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const getServerSideProps = async (ctx) => {
   const { id } = ctx.query;
+  // ユーザー情報の習得
   const API_URL = `https://jsonplaceholder.typicode.com/users/${id}`;
   const user = await fetch(API_URL);
   const userData = await user.json();
+  // ユーザーの投稿の習得
+  const POSTS_API_URL = `https://jsonplaceholder.typicode.com/posts?userId/${userData}`;
+  const posts = await fetch(POSTS_API_URL);
+  const postData = await posts.json();
 
   return {
     props: {
       fallback: {
         [API_URL]: userData,
+        [POSTS_API_URL]: postData,
       },
     },
   };
--- src/pages/users/index.jsx
+++ src/pages/users/index.jsx
@@ -2,17 +2,34 @@ import Head from "next/head";
 import { Inter } from "next/font/google";
 import { Header } from "@/src/components/Header";
 import { UsersComponent } from "@/src/components/Users";
+import { SWRConfig } from "swr";
 
 const inter = Inter({ subsets: ["latin"] });
+export const getServerSideProps = async () => {
+  const USERS_API_URL = `https://jsonplaceholder.typicode.com/users/`;
+  const users = await fetch(USERS_API_URL);
+  const usersData = await users.json();
 
-const Users = () => {
+  return {
+    props: {
+      fallback: {
+        [USERS_API_URL]: usersData,
+      },
+    },
+  };
+};
+
+const Users = (props) => {
+  const { fallback } = props;
   return (
     <>
       <Head>
         <title>Users Page</title>
       </Head>
-      <Header />
-      <UsersComponent />
+      <SWRConfig value={{ fallback }}>
+        <Header />
+        <UsersComponent />
+      </SWRConfig>
     </>
   );
 };
